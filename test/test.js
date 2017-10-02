f07c37b4816ab472f09ee112ceb3c26913fc8b36
CamilleINGOUF <ingouf.camille@gmail.com>
2017-10-02 11:21:40 +0200

    Scenar 2 test fail

--- test/test.js
+++ test/test.js
@@ -3,8 +3,14 @@
 var LyngkTestCase = TestCase("LyngkTestCase");
 
 //Rechercher une coordonnée qui n'existe pas -> A1
-LyngkTestCase.prototype.testB = function()
+LyngkTestCase.prototype.testA = function()
 {
     var coord = new Lyngk.Coordinates('A',1);
     assertFalse(coord.isValid());
+};
+
+LyngkTestCase.prototype.testB = function()
+{
+    var count = 0;
+    assertTrue(count == 43);
 };
