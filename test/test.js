d782ff9cf939eac4985ea6b9b0ab92315e6f29f7
CamilleINGOUF <ingouf.camille@gmail.com>
2017-10-02 10:20:01 +0200

    Scenar 1 test fail

--- test/test.js
+++ test/test.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.testA = function()
+{
+    var coord = new Lyngk.Coordinates('A',1);
+    assertTrue(coord.isValid());
+};
