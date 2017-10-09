9b3138e29da049f06298c99c66ca894780e73b34
CamilleINGOUF <ingouf.camille@gmail.com>
2017-10-09 11:25:37 +0200

    Scenar 11 test success

--- src/Engine.js
+++ src/Engine.js
@@ -3,5 +3,40 @@
 // enums definition
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
 
-Lyngk.Engine = function () {
+Lyngk.Engine = function ()
+{
+    var coordinatesIntersections = [];
+
+    var init = function()
+    {
+        var validCoord = Lyngk.goodCoordinates;
+        for(var i = 0; i < validCoord.length; i++)
+        {
+            coordinatesIntersections[validCoord[i]] = new Lyngk.Intersection();
+        }
+    }
+
+    this.init_one_piece = function()
+    {
+        for (var coord in coordinatesIntersections) {
+            if (coordinatesIntersections.hasOwnProperty(coord))
+            {
+                coordinatesIntersections[coord].pose(Lyngk.Color.IVORY);
+            }
+        }
+    }
+
+    this.is_full_one_piece = function()
+    {
+        for (var coord in coordinatesIntersections) {
+            if (coordinatesIntersections.hasOwnProperty(coord))
+            {
+                if(coordinatesIntersections[coord].getState() != Lyngk.State.ONE_PIECE)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    init();
 };
