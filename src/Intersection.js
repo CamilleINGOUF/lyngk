474b8a9e9f06a4ddef1dbe3a4280dd8e52cd8778
CamilleINGOUF <ingouf.camille@gmail.com>
2017-10-09 09:23:26 +0200

    Scenar 7 test success

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,5 +2,12 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c) {
+Lyngk.Intersection = function (c)
+{
+    var state = Lyngk.State.VACANT;
+
+    this.getState = function()
+    {
+        return state;
+    }
 };
