5c2b152013a8682338931b9289eae3da1b31adc5
CamilleINGOUF <ingouf.camille@gmail.com>
2017-10-09 09:42:05 +0200

    Scenar 8 test success

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,12 +2,24 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function (c)
+Lyngk.Intersection = function ()
 {
     var state = Lyngk.State.VACANT;
+    var piece;
 
     this.getState = function()
     {
         return state;
+    }
+
+    this.getPiece = function()
+    {
+        return piece;
+    }
+
+    this.pose = function(c)
+    {
+        piece = new Lyngk.Piece(c);
+        state = Lyngk.State.ONE_PIECE;
     }
 };
